9f0028e75a6e31f8dff3d42ff33fb1ebbb76e1be
KingBaum <bergerfranek@gmail.com>
2019-03-27 19:17:52 +0100

    Initialized project

---
+++ src/main/java/pl/polsl/ksiegarnia/HelloWorld.java
@@ -0,0 +1,13 @@
+package pl.polsl.ksiegarnia;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+    @RequestMapping("/")
+    public String index(){
+        return "Hello World";
+    }
+}
